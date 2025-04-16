96ca9aec9d2427593b256bb16b8350a2f159ed54
Abhishek <abhisheksharma270820@gmail.com>
2025-04-16 22:19:24 +0530

    Make changes on Navbar and Footer also set Background color of website

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-      <p>this is footer</p>
+    <footer className='bg-gray-900 text-white'>
+      <div className='px-4 h-16 flex justify-center items-center'>
+        <p>Copyright &copy; Get me A Chai - All rights reserved!</p>
+      </div>
     </footer>
   )
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        Navbar
+    <nav className='bg-gray-900 text-white'>
+      <div className='flex justify-between items-center px-4 h-16'>
+        <div className="logo font-bold text-xl">GetMeAChai!</div>
+        <ul className='flex gap-2'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Sign UP</li>
+          <li>Login</li>
+        </ul>
       </div>
     </nav>
   )
