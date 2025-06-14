693113bc18545028b1d0f7364eb342db3d66e3cd
Abhishek <abhisheksharma270820@gmail.com>
2025-06-14 11:29:56 +0530

    Set the callback url after the payment is done

--- components/Paymentpage.js
+++ components/Paymentpage.js
@@ -69,7 +69,7 @@ const Paymentpage = ({ username }) => {
             "description": "Test Transaction",
             "image": "https://example.com/your_logo",
             "order_id": orderId, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-            "callback_url": `http://localhost:3000/api/razorpay`,
+            "callback_url": `${process.env.NEXT_PUBLIC_URL}/api/razorpay`,
             "prefill": {
                 "name": "Gaurav Kumar",
                 "email": "gaurav.kumar@example.com",
